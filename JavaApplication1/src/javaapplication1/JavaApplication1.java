447e5697d158ac2bd0c2aca688361351676edf37
miqueas04 <80845205+miqueas04@users.noreply.github.com>
2023-03-20 08:27:42 -0300

    logica igu

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -1,11 +1,18 @@
 
 package javaapplication1;
 
+import javaapplication1.igu.Principal;
+
 
 public class JavaApplication1 {
                   
     public static void main(String[] args) {
-        
+        Principal princi = new Principal();
+        princi.setVisible(true);
+        princi.setLocationRelativeTo(null);
     }
+   
     
+    
+   
 }
